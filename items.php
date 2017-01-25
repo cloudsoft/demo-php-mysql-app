ba0a7b4ca3b64e6d7ec607f21e095ee03fa2596e
Robert Moss <robert.moss@cloudsoftcorp.com>
2017-01-25 17:40:57 +0000

    quote host ip

--- items.php
+++ items.php
@@ -1,11 +1,10 @@
 <?php
-$servername = 127.0.0.1;
+$servername = "127.0.0.1";
 $username = "${MYSQL_USERNAME}";
 $password = "${MYSQL_PASSWORD}";
 
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
